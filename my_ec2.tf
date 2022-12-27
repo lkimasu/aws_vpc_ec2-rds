55cacf2368108ab9caa7dbb7c6b18a3fc8feb0b9
lkimasu <lwlstjdl@naver.com>
2022-12-27 10:51:37 +0900

    fisrt commit

---
+++ my_ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_key_pair" "web_admin" {
+  key_name = "web_admin"
+  public_key = file("~/.ssh/web_admin.pub")
+}
+
+resource "aws_security_group" "my_security" {
+  name = "allow_ssh_from_all"
+  description = "Allow SSH,HTTP allow"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
