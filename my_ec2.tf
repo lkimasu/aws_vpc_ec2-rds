3e03bd06c8a20e5a19ea2d577b6650992a5fb690
lkimasu <lwlstjdl@naver.com>
2022-12-27 11:23:34 +0900

    Add ec2 creation

--- my_ec2.tf
+++ my_ec2.tf
@@ -3,6 +3,16 @@ resource "aws_key_pair" "web_admin" {
   public_key = file("~/.ssh/web_admin.pub")
 }
 
+resource "aws_instance" "webserver" {
+ ami = "ami-0bba69335379e17f8"
+ instance_type = "t2.micro"
+ key_name = aws_key_pair.web_admin.key_name
+ subnet_id = aws_subnet.Public-Subnet-A.id
+ vpc_security_group_ids = [
+ aws_security_group.my_security.id
+ ]
+}
+
 resource "aws_security_group" "my_security" {
   name = "allow_ssh_from_all"
   description = "Allow SSH,HTTP allow"
