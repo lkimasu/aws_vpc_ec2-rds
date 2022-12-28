3802e1aeaed5d700c286ed79c78bb46896783ca7
lkimasu <lwlstjdl@naver.com>
2022-12-28 11:11:06 +0900

    update rds security,databases name

--- my_db.tf
+++ my_db.tf
@@ -1,12 +1,14 @@
 resource "aws_db_instance" "my_db" {
   allocated_storage = 8
+  db_name = "my_db"
   engine = "mysql"
   engine_version = "8.0.28"
   instance_class = "db.t2.micro"
   username = "username"
   password = "password"
   skip_final_snapshot = true
-  db_subnet_group_name = aws_db_subnet_group.my_db_subnet.name	
+  db_subnet_group_name = aws_db_subnet_group.my_db_subnet.name
+  vpc_security_group_ids = [aws_security_group.my_db_security.id]	
 }
 
 resource "aws_security_group" "my_db_security" {
